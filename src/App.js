d97669871fb281244a354ca07df21fbdc5f2f831
Sandip Ghimire <ghimiresandip51@gmail.com>
2022-04-21 00:37:33 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -9,32 +9,24 @@ class App extends React.Component {
     return (
       <div className="card">
         <div className="header">
-          <div className="logo">
-            <a href=".">LOGO</a>
-          </div>
           <div className="social">
-            <a href="https://facebook.com" title="Facebook" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.facebook.com/loadnepalz" title="Facebook" target="_blank" rel="noopener noreferrer">
               <IconFacebook className="icon" />
             </a>
-            <a href="https://twitter.com" title="Twitter" target="_blank" rel="noopener noreferrer">
-              <IconTwitter className="icon" />
-            </a>
-            <a href="https://github.com/arkn98/coming-soon" title="GitHub" target="_blank" rel="noopener noreferrer">
-              <IconGithub className="icon" />
-            </a>
+            
           </div>
         </div>
         <div className="content">
           <div className="title-holder">
-            <h1>Get ready for the change.</h1>
-            <p>Website coming soon. Please check back to know more. Shoot us an email if you're curious.</p>
+            <h1>Website Loading Soon...</h1>
+            <p>Meanwhile,Shoot us an email if you're curious.</p>
           </div>
-          <a href="mailto:user@example.com">
+          <a href="mailto:loadnepal01@gmail.com">
             <div className="cta">Send us an email</div>
           </a>
         </div>
         <div className="footer">
-          <span>made by <a className="underlined" href="https://github.com/arkn98" target="_blank" rel="noopener noreferrer">arkn98</a> using <a className="underlined" href="https://reactjs.org/" title="ReactJS" target="_blank" rel="noopener noreferrer">React</a> | <a className="underlined" href="https://github.com/arkn98/coming-soon" title="GitHub repo" target="_blank" rel="noopener noreferrer">GitHub</a></span>
+          <h3>All rights Reserved @zerone intertech pvt. ltd</h3>
         </div>
       </div>
     );
