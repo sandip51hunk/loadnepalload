4f9d6472c7eed2acb23b663a9e80bd5e9c1b7774
Sandip Ghimire <ghimiresandip51@gmail.com>
2022-04-21 11:52:41 +0545

    add

--- src/App.js
+++ src/App.js
@@ -1,36 +1,48 @@
-import React from 'react';
-import { ReactComponent as IconFacebook } from './assets/icons/facebook.svg';
-import { ReactComponent as IconTwitter } from './assets/icons/twitter.svg';
-import { ReactComponent as IconGithub } from './assets/icons/github.svg';
+import React from "react";
+import { ReactComponent as IconFacebook } from "./assets/icons/facebook.svg";
+import { ReactComponent as IconTwitter } from "./assets/icons/twitter.svg";
+import { ReactComponent as IconGithub } from "./assets/icons/github.svg";
 import "./App.css";
 
 class App extends React.Component {
   render = () => {
     return (
       <div className="card">
-        <div className="header">
+        <div div className="header">
           <div className="social">
-            <a href="https://www.facebook.com/loadnepalz" title="Facebook" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://www.facebook.com/loadnepalz"
+              title="Facebook"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <IconFacebook className="icon" />
             </a>
-            
           </div>
         </div>
+        <div className="logo">
+          <img src="./logo.jpg" />
+        </div>
+
         <div className="content">
           <div className="title-holder">
-            <h1>Website Loading </h1>
-            <p>Meanwhile,Shoot us an email if you're curious.</p>
+            <h1>Website Loading Soon... </h1>
+            Meanwhile,Shoot us an email if you're curious.
           </div>
+          
           <a href="mailto:loadnepal01@gmail.com">
-            <div className="cta">Send us an email</div>
-          </a>
+                <div className="cta">Send us an email</div>
+              </a>
         </div>
+
+
+
         <div className="footer">
           <h3>All rights Reserved @zerone intertech pvt. ltd</h3>
         </div>
       </div>
     );
-  }
+  };
 }
 
 export default App;
